51645385e7632928e3edc03584c400e25a9a8305
eyahya-khan <eyahyakhan@gmail.com>
2022-02-23 12:14:47 +0100

    final update

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -6,3 +6,13 @@ export const hackerSpeak = (words) => {
               .replace(/o/gi,'0')
               .replace(/s/gi,'5');
 }
+
+
+
+/*
+export const hackerSpeak = (words) => {
+  words = words.replace(/[aeios]/g, charactersToReplace=>({'a':'4','e':'3','i':'1','o':'0','s':'5'})
+  [charactersToReplace]);  
+    return words
+  }
+*/  
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,5 +1,23 @@
+/*
 export const hashtags = (text) => {
   const list = text.split(' ');
   return list.filter(item => item.substring(0,1) === "#");
   
 }
+*/
+
+export const hashtags = (text) => {
+  return text.split(' ').filter(word => word.startsWith('#'))
+}
+
+
+/*
+export const hashtags = (text) => text = text.match(/#\w+/gm);
+*/
+
+
+/*
+export const hashtags = (text) => {
+  return text.split(" ").filter((word) => word.charAt(0) === "#");
+};
+*/
